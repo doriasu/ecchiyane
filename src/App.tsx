5069b9639af355f7dd68c455d4007e4b46ee1744
doriasu <takuandtetsu@live.jp>
2020-06-25 20:04:36 +0900

    twitter認証の基礎中の基礎の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase,{providerTwitter} from "./base";
+import {Button} from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      えっちやね〜診断
-    </div>
-  );
+class App extends React.Component{
+    loginWithTwitter() {
+        firebase.auth().signInWithRedirect(providerTwitter).then(function(result) {
+            // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+            // You can use these server side with your app's credentials to access the Twitter API.
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    };
+    render(){
+        return (
+            <div>
+                <div>
+                    略
+                    <Button onClick={this.loginWithTwitter}>Login</Button>
+                </div>
+            </div>
+        );
+    }
 }
-
 export default App;
